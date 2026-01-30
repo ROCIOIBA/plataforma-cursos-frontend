8f7c7e40ea516d7718d3284bc0ed8dd023cc5094
rocioibañez <mirta_gayoso@hotmail.com>
2026-01-30 13:44:28 -0300

    Fix: correct API baseURL to /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://plataforma-cursos-backend-4o3v.onrender.com",
+  baseURL: "https://plataforma-cursos-backend-4o3v.onrender.com/api",
 });
 
 export default api;
