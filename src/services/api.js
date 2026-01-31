bde1062679451c510429e2ae3d9acace80c8a3f9
rocioibañez <mirta_gayoso@hotmail.com>
2026-01-31 11:59:58 -0300

    Frontend apuntando al backend de Render

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   // LOCAL
-  baseURL: "http://localhost:3000/api",
+  //baseURL: "http://localhost:3000/api",
 
   // RENDER
-  //baseURL: "https://plataforma-cursos-backend-4o3v.onrender.com/api",
+  baseURL: "https://plataforma-cursos-backend-4o3v.onrender.com/api",
 
   withCredentials: true
 });
