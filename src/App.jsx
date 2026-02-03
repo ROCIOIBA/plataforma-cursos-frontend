dac741ff785a0306fdc389e3090918c0b1631895
rocioibañez <mirta_gayoso@hotmail.com>
2026-02-03 19:15:19 -0300

    Fix: remover BrowserRouter duplicado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import RutaPrivada from "./components/RutaPrivada";
@@ -13,7 +11,7 @@ import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
       <Routes>
@@ -34,6 +32,6 @@ export default function App() {
       </Routes>
 
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
