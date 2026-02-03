82064ce3e94e5ce799f2dd050ce72bc6a8583d03
rocioibañez <mirta_gayoso@hotmail.com>
2026-02-03 18:22:42 -0300

    Fix: App.jsx no se importa a sí mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "./styles.css"; // ← IMPORTANTE: carga tus estilos globales
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import RutaPrivada from "./components/RutaPrivada";
+
+import Home from "./pages/Home";
+import Cursos from "./pages/Cursos";
+import MisCursos from "./pages/MisCursos";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+export default function App() {
+  return (
     <BrowserRouter>
-      <App />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cursos" element={<Cursos />} />
+
+        <Route
+          path="/mis-cursos"
+          element={
+            <RutaPrivada>
+              <MisCursos />
+            </RutaPrivada>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  );
+}
